c2bd2f12318df712067e6136bcb767c338a7c90d
Tarun <Tarun@192.168.0.12>
2016-01-31 11:41:28 -0600

    Grabber can now be controlled by a joystick button, but also be stopped using the joystick itself OR using a limit switch

--- AMESRobot2016/src/org/usfirst/frc/team3243/robot/Grabber.java
+++ AMESRobot2016/src/org/usfirst/frc/team3243/robot/Grabber.java
@@ -1,19 +1,26 @@
 package org.usfirst.frc.team3243.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Victor;
 
 public class Grabber {
 	Victor cimBall;
+	DigitalInput limitswitch;
 	Grabber(){
 		Victor cimBall = new Victor(4);
+		DigitalInput limitswitch = new DigitalInput(1/*whatever port it will use*/);
 	}
 	
 void pushIn(int buttonCount){//Move this to grabber class.
 		if (buttonCount % 2 == 1)
 		{
+			while (limitswitch.get())
+			{
 		cimBall.set(0.2);
+			}
 		}
-		else cimBall.set(0);
+		else cimBall.set(0.2);
+		
 	}
 	void ballCarrier(int direction){//Move this to grabber class.
 		
