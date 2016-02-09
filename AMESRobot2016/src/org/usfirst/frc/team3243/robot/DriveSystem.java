64e8c5665e771191b9b7f4a36b2d502ad916b257
Mark <asdf@asdf.com>
2016-02-08 19:28:58 -0700

    did some siege bot coding

--- AMESRobot2016/src/org/usfirst/frc/team3243/robot/DriveSystem.java
+++ AMESRobot2016/src/org/usfirst/frc/team3243/robot/DriveSystem.java
@@ -44,10 +44,10 @@ public class DriveSystem {
 	 * @param drv
 	 */
 	void drive(double[] drv){
-		cim1.set(drv[0]);
-		cim2.set(drv[0]);//-drv[0]);
-		cim3.set(-drv[1]);//drv[1]);
-		cim4.set(-drv[1]);
+		cim1.set(-drv[1]);
+		cim2.set(-drv[1]);//-drv[0]);
+		cim3.set(drv[0]);//drv[1]);
+		cim4.set(drv[0]);
 	}
 	
 	void quickdrive(double[] drv){
